77a1c0987deff895ddff84ee90623f52dd3aee6c
shrutidy16 <105289516+shrutidy16@users.noreply.github.com>
2023-01-06 22:30:25 +0530

    added starting page

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Home from './components/home/Home'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  
+} from "react-router-dom"
+function App() {
+  return (
+    <Router>
+          <Routes>
+          
+            <Route path='/' element={<Home />} />
+           
+          </Routes>
+    </Router>
+  );
+}
+
+export default App;
